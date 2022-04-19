1f796c7dd72296a31f2629c0c2f61ff97bcad51e
phdtalk <rubic992@gmail.com>
2022-04-19 16:00:08 +0200

    Create R_functions.R

---
+++ R_functions.R
@@ -0,0 +1,41 @@
+####Extract the data from venn list; see the venn list example below
+# x <- list(
+#   upRNA = upRNA$GeneName, 
+#   upProt = upProt$GeneName, 
+#   downRNA = downRNA$GeneName,
+#   downProt = downProt$GeneName
+#   )
+
+Intersect <- function (x) {  
+  # Multiple set version of intersect
+  # x is a list
+  if (length(x) == 1) {
+    unlist(x)
+  } else if (length(x) == 2) {
+    intersect(x[[1]], x[[2]])
+  } else if (length(x) > 2){
+    intersect(x[[1]], Intersect(x[-1]))
+  }
+}
+
+Union <- function (x) {  
+  # Multiple set version of union
+  # x is a list
+  if (length(x) == 1) {
+    unlist(x)
+  } else if (length(x) == 2) {
+    union(x[[1]], x[[2]])
+  } else if (length(x) > 2) {
+    union(x[[1]], Union(x[-1]))
+  }
+}
+
+Setdiff <- function (x, y) {
+  # Remove the union of the y's from the common x's. 
+  # x and y are lists of characters.
+  xx <- Intersect(x)
+  yy <- Union(y)
+  setdiff(xx, yy)
+}
+
+
