2d98b77b147b03f6169bb291d121f480995e0551
phdtalk <rubic992@gmail.com>
2022-04-19 16:52:39 +0200

    Update R_functions.R

--- R_functions.R
+++ R_functions.R
@@ -38,4 +38,24 @@ Setdiff <- function (x, y) {
   setdiff(xx, yy)
 }
 
-
+####Clean up theme from Michi
+cleanup = theme(panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank(),
+                axis.line = element_line(color = "black", lineend = "square", size =0.7),
+                plot.margin = margin(0.5, 0.5, 0.5, 2, "cm"),
+                axis.ticks = element_line(size = 0.7, lineend = "square", color = "black"),
+                axis.ticks.length = unit(.1,"cm"),
+                axis.title.x = element_text(color = "black", face = "bold", size = fsize, vjust = -2),
+                axis.title.y = element_text(color = "black", face = "bold", size = fsize, vjust = 5),
+                axis.text.x = element_text(color = "black", size = fsize*0.75, face = NULL, 
+                                           hjust = NULL, vjust = NULL, angle = NULL,
+                                           margin = margin(5,0,0,0,"pt")),
+                axis.text.y = element_text(color = "black", size = fsize*0.75,
+                                           margin = margin(0,5,0,0,"pt")),
+                legend.background = element_blank(),
+                legend.key = element_blank(),
+                legend.title = element_text(color = "black", face = "bold", size = fsize),
+                legend.text = element_text(color = "black", size = fsize*0.9),
+                plot.title = element_text(size = fsize*1.25),
+                legend.position = "right")
